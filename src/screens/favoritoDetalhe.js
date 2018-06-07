7774d62cda7782ef22262748b40c949ad249fa05
Werliton <werliton.carlos@ceuma.br>
2018-06-07 11:20:17 -0300

    Finalizando tela de detalhe

--- src/screens/favoritoDetalhe.js
+++ src/screens/favoritoDetalhe.js
@@ -4,15 +4,17 @@ import {
     Text,
     View, Button, Alert
   } from 'react-native'
-import { Container, Header, Content, Form, Item, Input, Title, List, ListItem, Thumbnail, Body } from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Title, List, ListItem, Thumbnail, Body, Icon, Right } from 'native-base';
 
 export default class FavoritoDetalhe extends Component {
       constructor(props){
           super(props)
           this.state = {
               atletas:[],
+              clubes:[],
               capitao:'',
-              pontos:''
+              pontos:'',
+              escudo:''
           }
       }
 
@@ -26,21 +28,40 @@ export default class FavoritoDetalhe extends Component {
             response.atletas.forEach(atleta =>{
                 atletas.push(atleta)
             })
-
+            // Ordenando lista de atletas
+            atletas =  atletas.sort((a, b )=> a.posicao_id - b.posicao_id)
             this.setState({
                 atletas,
                 capitao:response.capitao_id,
                 pontos:response.pontos
             })
-            
+            return response
         })
-        .then()
+        .then(response => {
+            let clubes = []
+            Object.keys(response.clubes).map(element =>{
+                clubes.push(response.clubes[element])
+            })         
+
+            this.setState({clubes})
+        })
         .catch(error => Alert.alert('Busca de atletas','Erro ao buscar atletas:'+error))
     }
 
     isCapita(atletaId){        
         if(atletaId === this.state.capitao)
             return <Text style={{color:'#f4a041', fontWeight:'bold'}}>Capita</Text>
+    }
+
+    renderEscudo(clubeId){
+        let clubes = this.state.clubes.filter(clube => clube.id === clubeId) 
+        let escudos = []
+        clubes.forEach(clubes =>{
+            Object.keys(clubes.escudos).map(item =>{
+                escudos.push(clubes.escudos[item])
+            })
+        })
+        return <Thumbnail square size={20} source={{ uri: escudos[0] }} />
     }
 
     render(){
@@ -52,11 +73,14 @@ export default class FavoritoDetalhe extends Component {
                         {
                             this.state.atletas.map((atleta, key) => 
                                 <ListItem key={key}>
-                                    <Thumbnail square size={80} source={{ uri: atleta.foto }} />
+                                    {this.renderEscudo(atleta.clube_id)}
                                     <Body>
                                         <Text>{atleta.apelido} {this.isCapita(atleta.atleta_id)} </Text>
-                                        <Text note>Média: {atleta.media_num}</Text>
                                     </Body>
+                                    <Right>
+                                        <Text note>Média: {atleta.media_num}</Text>
+                                        <Icon name="arrow-forward" />
+                                    </Right>
                                 </ListItem>
                             )        
                         }
