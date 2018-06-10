2aeb2c95079f69fadc15f6b74d788710b279be1a
werliton <werlitoncarlos@gmail.com>
2018-06-10 14:41:00 -0300

    Incluindo loading na tela de parciais

--- src/screens/parciaisJogadores.js
+++ src/screens/parciaisJogadores.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import {
-    Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Title
+    Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Title, Spinner
    } from 'native-base'
 
   export default class ParciaisJogadores extends Component {
     constructor(props){
         super(props)
         this.state = {
-            jogadores:[]
+            jogadores:[], loading: true
         }
     }
   
@@ -20,10 +20,33 @@ import {
             Object.keys(resp.atletas).map( item => {
                 atletas.push(resp.atletas[item])
             })
-            this.setState({jogadores:atletas})
+            this.setState({jogadores:atletas, loading:false})
         })
         .catch(error => Alert.alert('Busca de jogadores','Erro ao buscar jogadores'))
     } 
+
+    renderContent(){
+        if(this.state.loading){
+            return <Spinner />
+        }
+        return (
+            this.state.jogadores.map(jogador => 
+                <List>
+                    <ListItem avatar>
+                        <Left>
+                            <Thumbnail source={{ uri: `${jogador.foto}` }} />
+                        </Left>
+                        <Body>
+                            <Text>{jogador.apelido}</Text>
+                        </Body>
+                        <Right>
+                            <Text note>{jogador.pontuacao}</Text>
+                        </Right>
+                    </ListItem>
+                </List>
+            )
+        )
+    }
 
     render(){
         return(
@@ -32,24 +55,7 @@ import {
                     <Title>Parciais dos Jogadores</Title>
                 </Header>
                 <Content>
-                    {
-                        this.state.jogadores.map(jogador => 
-                            <List>
-                                <ListItem avatar>
-                                    <Left>
-                                        <Thumbnail source={{ uri: `${jogador.foto}` }} />
-                                    </Left>
-                                    <Body>
-                                        <Text>{jogador.apelido}</Text>
-                                    </Body>
-                                    <Right>
-                                        <Text note>{jogador.pontuacao}</Text>
-                                    </Right>
-                                </ListItem>
-                            </List>
-                        )
-                    }
-                    
+                    {this.renderContent()}
                 </Content>
             </Container>
         )
